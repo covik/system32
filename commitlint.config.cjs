6482d6a141f2651f7fef046f68cfb53f2a8fc911
covik <6125218+Covik@users.noreply.github.com>
2023-05-26 19:43:58 +0200

    chore(tooling): configure commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'workspace', // anything applied to whole repo
+        'deps',
+        'tooling',
+        'production',
+        'local',
+      ],
+    ],
+  },
+};
