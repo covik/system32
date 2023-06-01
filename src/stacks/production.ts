9cb5daed3b9d57769abb303d67500683fb4fcbf9
covik <6125218+Covik@users.noreply.github.com>
2023-06-01 13:22:19 +0200

    feat(production): move new frontend to subdomain 'next.zarafleet.com'

--- src/stacks/production.ts
+++ src/stacks/production.ts
@@ -13,7 +13,7 @@ class Domain {
   public static primary = 'zarafleet.com';
   public static frontend = 'app.zarafleet.com';
   public static traccar = 'old.zarafleet.com';
-  public static newFrontend = 'new.zarafleet.com';
+  public static newFrontend = 'next.zarafleet.com';
 }
 
 class Project {
