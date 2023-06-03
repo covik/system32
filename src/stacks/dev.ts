bc26a1c41151f28c1ea5630b87b61fb3cee0aa1d
covik <6125218+Covik@users.noreply.github.com>
2023-06-03 22:33:41 +0200

    fix(dev): change instances of 'test' to 'dev'

--- src/stacks/dev.ts
+++ src/stacks/dev.ts
@@ -1,7 +1,7 @@
 import * as digitalocean from '@pulumi/digitalocean';
-import * as k8s from '@pulumi/kubernetes';
 import * as pulumi from '@pulumi/pulumi';
 import * as backend from '../resources/backend';
+import * as cluster from '../resources/cluster';
 import * as frontend from '../resources/frontend';
 import * as mysql from '../resources/mysql';
 import * as traefik from '../resources/traefik';
@@ -13,88 +13,86 @@ import {
 export function resources(): void {
   const config = new pulumi.Config();
 
+  const region = config.require('region');
+  const httpNodePort = 32080;
+  const teltonikaNodePort = 32027;
+
   const vpc = new digitalocean.Vpc('primary-vpc', {
-    name: 'fms-test',
-    region: 'fra1',
+    name: 'fms-dev',
+    region,
   });
 
-  const cluster = new digitalocean.KubernetesCluster('primary-cluster', {
-    name: 'fms-test',
-    region: 'fra1',
-    version: '1.27.2-do.0',
-    nodePool: {
-      name: 'test-worker',
-      size: 's-1vcpu-2gb',
-      nodeCount: 1,
-      autoScale: false,
+  const kubernetes = new cluster.DigitalOceanCluster(
+    'primary-kubernetes-cluster',
+    {
+      name: 'fms-dev',
+      version: '1.27.2-do.0',
+      vmSize: 's-1vcpu-2gb',
+      nodePoolName: 'dev-worker',
+      nodePoolTags: [],
+      token: config.requireSecret('k8s-cluster-token'),
+      region,
+      vpc,
     },
-    vpcUuid: vpc.id,
-    ha: false,
-    autoUpgrade: false,
-    surgeUpgrade: true,
-  });
+  );
 
   new digitalocean.Project('primary-project', {
-    name: 'FMS-test',
+    name: 'FMS Dev',
     environment: 'Development',
     description: 'Infrastructure for Zara Fleet Management System',
     purpose: 'Web Application',
-    resources: [cluster.clusterUrn],
+    resources: [kubernetes.cluster.clusterUrn],
   });
 
-  const ipAddress = getNodeIpAddress(cluster);
+  const ipAddress = getNodeIpAddress(kubernetes.cluster);
   const primaryDomainId = getRootDomainId();
 
   const domain = new digitalocean.DnsRecord('domain', {
-    name: 'test',
+    name: 'dev',
     domain: primaryDomainId,
     value: ipAddress,
     type: 'A',
   });
 
-  const provider = new k8s.Provider('do-cluster', {
-    kubeconfig: cluster.kubeConfigs[0].rawConfig,
-    enableServerSideApply: true,
-  });
-
   const kubernetesComponentOptions = {
     providers: {
-      kubernetes: provider,
+      kubernetes: kubernetes.provider,
     },
   };
 
   const containerRegistryCredentials =
     createContainerRegistryCredentials(config);
 
-  const unstableFrontendImage = resolveRegistryImage(
+  const frontendImageNext = resolveRegistryImage(
     config.require('frontend-unstable-image'),
     config,
   );
 
   new traefik.IngressController(
     'traefik-ingress',
-    { nodePort: 30080 },
+    { nodePort: httpNodePort },
     kubernetesComponentOptions,
   );
 
   new frontend.Application(
-    'unstable-zara-fleet',
+    'zara-fleet-next',
     {
-      image: unstableFrontendImage,
+      image: frontendImageNext,
       containerRegistryCredentials,
+      namespace: 'frontend-next',
       hostname: domain.fqdn,
     },
     kubernetesComponentOptions,
   );
 
-  const databaseConnection = mysql.kubernetes('mysql:8', provider);
+  const databaseConnection = mysql.kubernetes('mysql:8', kubernetes.provider);
 
   new backend.Application(
     'traccar',
     {
       databaseConnection,
-      routes: ['test.zarafleet.com/api'],
-      teltonikaNodePort: 32027,
+      routes: ['dev.zarafleet.com/api'],
+      teltonikaNodePort,
     },
     kubernetesComponentOptions,
   );
