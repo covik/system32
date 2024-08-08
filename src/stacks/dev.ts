bfc5c9933ba83e3b1163ca05fe3c794a7d086804
Mate Nakić <mate.nakic@zth.dev>
2024-08-08 13:19:19 +0200

    feat: deploy new dev cluster on civo cloud
    Add monitoring and stremio app

--- src/stacks/dev.ts
+++ src/stacks/dev.ts
@@ -1,58 +1,40 @@
-import * as digitalocean from '@pulumi/digitalocean';
+import { writeFile } from 'node:fs/promises';
+import * as civo from '@pulumi/civo';
+import * as k8s from '@pulumi/kubernetes';
 import * as pulumi from '@pulumi/pulumi';
-import * as backend from '../resources/backend';
+import * as app from '../resources/app';
 import * as cluster from '../resources/cluster';
-import * as frontend from '../resources/frontend';
-import * as mysql from '../resources/mysql';
-import * as traefik from '../resources/traefik';
-import {
-  createContainerRegistryCredentials,
-  resolveRegistryImage,
-} from '../utils';
+import * as gateway from '../resources/gateway';
+import * as monitoring from '../resources/monitoring';
+import * as security from '../resources/security';
 
-export function resources(): void {
+const stremioUrl = '01911f8c-8698-7a3d-a960-5f15f55a668c.zth.dev';
+
+export function resources(): unknown {
   const config = new pulumi.Config();
 
-  const region = config.require('region');
-  const httpNodePort = 32080;
-  const teltonikaNodePort = 32027;
-
-  const vpc = new digitalocean.Vpc('primary-vpc', {
-    name: 'fms-dev',
-    region,
+  const network = new civo.Network('primary-vpc', {
+    label: 'hq',
   });
 
-  const kubernetes = new cluster.DigitalOceanCluster(
-    'primary-kubernetes-cluster',
-    {
-      name: 'fms-dev',
-      version: '1.27.4-do.0',
-      vmSize: 's-1vcpu-2gb',
-      nodePoolName: 'dev-worker',
-      nodePoolTags: [],
-      token: config.requireSecret('k8s-cluster-token'),
-      region,
-      vpc,
-    },
-  );
-
-  new digitalocean.Project('primary-project', {
-    name: 'FMS Dev',
-    environment: 'Development',
-    description: 'Infrastructure for Zara Fleet Management System',
-    purpose: 'Web Application',
-    resources: [kubernetes.cluster.clusterUrn],
+  const firewall = new civo.Firewall('primary-defense', {
+    name: 'abs',
+    networkId: network.id,
   });
 
-  const ipAddress = getNodeIpAddress(kubernetes.cluster);
-  const primaryDomainId = getRootDomainId();
-
-  const domain = new digitalocean.DnsRecord('domain', {
-    name: 'dev',
-    domain: primaryDomainId,
-    value: ipAddress,
-    type: 'A',
+  const kubernetes = new cluster.CivoCluster('primary-cluster', {
+    firewall,
+    name: 'engine',
+    network,
+    version: '1.29.2-k3s1',
+    vmSize: 'g4s.kube.medium',
   });
+
+  const kubeconfigPath = process.env['KUBECONFIG'];
+  if (kubeconfigPath)
+    kubernetes.kubeconfig.apply((kubeconfig) =>
+      writeFile(kubeconfigPath, kubeconfig),
+    );
 
   const kubernetesComponentOptions = {
     providers: {
@@ -60,52 +42,420 @@ export function resources(): void {
     },
   };
 
-  const containerRegistryCredentials =
-    createContainerRegistryCredentials(config);
+  const gatewayAPI = installKubernetesGatewayAPI({
+    provider: kubernetes.provider,
+  });
 
-  const frontendImageNext = resolveRegistryImage(
-    config.require('frontend-unstable-image'),
-    config,
+  const gatewayController = new gateway.EnvoyGateway(
+    'envoy-gateway',
+    {},
+    {
+      ...kubernetesComponentOptions,
+      dependsOn: [gatewayAPI],
+    },
   );
 
-  new traefik.IngressController(
-    'traefik-ingress',
-    { nodePort: httpNodePort },
+  const gatewayConfig = {
+    name: 'default-gateway',
+    namespace: 'default',
+  };
+
+  const certManager = new security.CertManager(
+    'cert-manager',
+    {},
     kubernetesComponentOptions,
   );
 
-  new frontend.Application(
-    'zara-fleet-next',
+  const issuer = new k8s.apiextensions.CustomResource(
+    'letsencrypt-issuer',
     {
-      image: frontendImageNext,
-      containerRegistryCredentials,
-      namespace: 'frontend-next',
-      hostname: domain.fqdn,
+      apiVersion: 'cert-manager.io/v1',
+      kind: 'ClusterIssuer',
+      metadata: {
+        name: 'letsencrypt-prod',
+      },
+      spec: {
+        acme: {
+          server: 'https://acme-v02.api.letsencrypt.org/directory',
+          email: 'mate.nakic@zth.dev',
+          privateKeySecretRef: {
+            name: 'default-acme-private-key',
+          },
+          solvers: [
+            {
+              http01: {
+                gatewayHTTPRoute: {
+                  parentRefs: [
+                    {
+                      name: gatewayConfig.name,
+                      namespace: gatewayConfig.namespace,
+                      kind: 'Gateway',
+                    },
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    {
+      provider: kubernetes.provider,
+      dependsOn: [certManager],
+      deleteBeforeReplace: true,
+    },
+  );
+
+  const certificateSecretName = 'zth-dev-tls';
+  const certificate = new k8s.apiextensions.CustomResource(
+    'zth-dev-certificate',
+    {
+      apiVersion: 'cert-manager.io/v1',
+      kind: 'Certificate',
+      metadata: {
+        name: 'zth-dev',
+        namespace: gatewayConfig.namespace,
+      },
+      spec: {
+        secretName: certificateSecretName,
+        issuerRef: {
+          name: issuer.metadata.name,
+          kind: issuer.kind,
+        },
+        dnsNames: ['zth.dev', stremioUrl],
+      },
+    },
+    {
+      provider: kubernetes.provider,
+      deleteBeforeReplace: true,
+    },
+  );
+
+  const gatewayInstance = new k8s.apiextensions.CustomResource(
+    'primary-gateway',
+    {
+      apiVersion: 'gateway.networking.k8s.io/v1',
+      kind: 'Gateway',
+      metadata: {
+        name: gatewayConfig.name,
+        namespace: gatewayConfig.namespace,
+      },
+      spec: {
+        gatewayClassName: gatewayController.gatewayClassName,
+        listeners: [
+          {
+            name: 'http',
+            protocol: 'HTTP',
+            port: 80,
+            allowedRoutes: {
+              namespaces: { from: 'All' },
+              kinds: [{ kind: 'HTTPRoute' }],
+            },
+          },
+          {
+            name: 'https',
+            protocol: 'HTTPS',
+            port: 443,
+            tls: {
+              mode: 'Terminate',
+              certificateRefs: [
+                {
+                  name: certificateSecretName,
+                },
+              ],
+            },
+            allowedRoutes: {
+              namespaces: { from: 'All' },
+              kinds: [{ kind: 'HTTPRoute' }],
+            },
+          },
+          {
+            name: 'teltonika',
+            protocol: 'TCP',
+            port: 8400,
+            allowedRoutes: {
+              namespaces: { from: 'All' },
+              kinds: [{ kind: 'TCPRoute' }],
+            },
+          },
+        ],
+      },
+    },
+    {
+      provider: kubernetes.provider,
+      deleteBeforeReplace: true,
+      dependsOn: [certificate],
+    },
+  );
+
+  const appLabels = { app: 'nginx' };
+
+  const deployment = new k8s.apps.v1.Deployment(
+    'nginx-deployment',
+    {
+      metadata: { name: 'nginx' },
+      spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+          metadata: { labels: appLabels },
+          spec: {
+            containers: [
+              {
+                name: 'nginx',
+                image: 'nginx:latest',
+                ports: [{ containerPort: 80 }],
+              },
+            ],
+          },
+        },
+      },
+    },
+    {
+      provider: kubernetes.provider,
+    },
+  );
+
+  const service = new k8s.core.v1.Service(
+    'nginx-service',
+    {
+      metadata: { name: 'nginx' },
+      spec: {
+        ports: [
+          {
+            port: 80,
+            targetPort:
+              deployment.spec.template.spec.containers[0].ports[0]
+                .containerPort,
+          },
+        ],
+        selector: appLabels,
+      },
+    },
+    {
+      provider: kubernetes.provider,
+    },
+  );
+
+  new k8s.apiextensions.CustomResource(
+    'nginx-route',
+    {
+      apiVersion: 'gateway.networking.k8s.io/v1',
+      kind: 'HTTPRoute',
+      spec: {
+        parentRefs: [
+          {
+            name: gatewayInstance.metadata.name,
+            sectionName: 'https',
+          },
+        ],
+        hostnames: ['zth.dev'],
+        rules: [
+          {
+            matches: [
+              {
+                path: {
+                  type: 'Exact',
+                  value: '/',
+                },
+              },
+            ],
+            backendRefs: [
+              {
+                name: service.metadata.name,
+                port: service.spec.ports[0].port,
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      provider: kubernetes.provider,
+    },
+  );
+
+  const telemetryOperator = new monitoring.OpenTelemetryOperator(
+    'opentelemetry-operator',
+    {},
+    kubernetesComponentOptions,
+  );
+
+  const gcpServiceAccountSecret = config.requireSecret(
+    'gcp-otlp-service-account',
+  );
+
+  const gcpCredentials = new k8s.core.v1.Secret(
+    'gcp-credentials',
+    {
+      metadata: {
+        namespace: 'default',
+      },
+      data: {
+        'key.json': gcpServiceAccountSecret.apply((secret) =>
+          Buffer.from(secret).toString('base64'),
+        ),
+      },
+    },
+    {
+      provider: kubernetes.provider,
+    },
+  );
+
+  const kubeStateMetrics = new monitoring.KubeStateMetrics(
+    'ksm',
+    {},
+    kubernetesComponentOptions,
+  );
+
+  new k8s.apiextensions.CustomResource(
+    'node-metrics-collector',
+    {
+      apiVersion: 'opentelemetry.io/v1alpha1',
+      kind: 'OpenTelemetryCollector',
+      metadata: {
+        namespace: 'default',
+      },
+      spec: {
+        mode: 'daemonset',
+        config: pulumi.interpolate`
+          receivers:
+            hostmetrics:
+              collection_interval: 60s
+              scrapers:
+                cpu:
+                memory:
+                disk:
+                filesystem:
+                load:
+                network:
+            prometheus:
+              config:
+                scrape_configs:
+                  - job_name: kube-state-metrics
+                    scrape_interval: 60s
+                    static_configs:
+                      - targets:
+                        - ${kubeStateMetrics.serviceName}.svc.cluster.local:${kubeStateMetrics.servicePort}
+
+          processors:
+            batch:
+            memory_limiter:
+              limit_mib: 4000
+              spike_limit_mib: 500
+              check_interval: 5s
+
+          exporters:
+            googlecloud:
+              project: cromanjonac
+              user_agent: opentelemetry-collector
+              metric:
+                prefix: custom.googleapis.com/opentelemetry/
+
+          service:
+            pipelines:
+              metrics:
+                receivers: [hostmetrics, prometheus]
+                processors: [batch, memory_limiter]
+                exporters: [googlecloud]
+        `,
+        env: [
+          {
+            name: 'GOOGLE_APPLICATION_CREDENTIALS',
+            value: '/etc/gcp/key.json',
+          },
+        ],
+        volumeMounts: [
+          {
+            name: 'gcp-credentials',
+            mountPath: '/etc/gcp',
+            readOnly: true,
+          },
+        ],
+        volumes: [
+          {
+            name: 'gcp-credentials',
+            secret: {
+              secretName: gcpCredentials.metadata.name,
+            },
+          },
+        ],
+      },
+    },
+    {
+      provider: kubernetes.provider,
+      dependsOn: [telemetryOperator, gcpCredentials, kubeStateMetrics],
+    },
+  );
+
+  const stremio = new app.StremioServer(
+    'stremio',
+    {},
+    kubernetesComponentOptions,
+  );
+
+  new k8s.apiextensions.CustomResource(
+    'stremio-route',
+    {
+      apiVersion: 'gateway.networking.k8s.io/v1',
+      kind: 'HTTPRoute',
+      metadata: {
+        namespace: stremio.namespaceName,
+      },
+      spec: {
+        parentRefs: [
+          {
+            name: gatewayInstance.metadata.name,
+            namespace: gatewayInstance.metadata.namespace,
+            sectionName: 'https',
+          },
+        ],
+        hostnames: [stremioUrl],
+        rules: [
+          {
+            matches: [
+              {
+                path: {
+                  type: 'PathPrefix',
+                  value: '/',
+                },
+              },
+            ],
+            backendRefs: [
+              {
+                name: stremio.serviceName,
+                port: stremio.servicePort,
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      provider: kubernetes.provider,
+    },
+  );
+
+  /*new backend.TeltonikaServer(
+    'teltonika',
+    {
+      containerRegistryCredentials: createContainerRegistryCredentials(config),
+      image:
+        'ghcr.io/sudocovik/fms-backend:latest@sha256:c472e7ce6da6f2d335056271480c379d9d1603d89ca19b536a1fc1073c8e69c9',
+      gatewayClassName: gateway.gatewayClassName,
     },
     kubernetesComponentOptions,
-  );
+  );*/
 
-  const databaseConnection = mysql.kubernetes('mysql:8', kubernetes.provider);
+  return {};
+}
 
-  new backend.Application(
-    'traccar',
+function installKubernetesGatewayAPI({ provider }: { provider: k8s.Provider }) {
+  return new k8s.yaml.ConfigFile(
+    'gateway-api-crd',
     {
-      databaseConnection,
-      routes: ['dev.zarafleet.com/api'],
-      teltonikaNodePort,
+      file: 'https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/experimental-install.yaml',
     },
-    kubernetesComponentOptions,
+    { provider },
   );
-}
-
-function getNodeIpAddress(
-  cluster: digitalocean.KubernetesCluster,
-): pulumi.Output<string> {
-  return cluster.nodePool.nodes[0].dropletId.apply((id) =>
-    digitalocean.getDropletOutput({ id: Number(id) }),
-  ).ipv4Address;
-}
-
-function getRootDomainId(): pulumi.Output<string> {
-  return digitalocean.getDomainOutput({ name: 'zarafleet.com' }).id;
 }
