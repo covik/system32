0b6ed3431d14008184707af0af49d78893937017
Mate Nakić <mate.nakic@zth.dev>
2024-09-02 21:06:04 +0000

    feat: enhance security with dnssec, dkim, spf and dmarc

--- src/stacks/staging.ts
+++ src/stacks/staging.ts
@@ -69,6 +69,10 @@ export function resources(): unknown {
     },
   });
 
+  new cloudflare.ZoneDnssec('zth-dev-dnssec', {
+    zoneId: dnsZone.id,
+  });
+
   const mxRecords = [
     { name: 'zth.dev', priority: 10, value: 'alt3.aspmx.l.google.com.' },
     { name: 'zth.dev', priority: 10, value: 'alt4.aspmx.l.google.com.' },
@@ -88,13 +92,40 @@ export function resources(): unknown {
     });
   });
 
-  new cloudflare.Record('zth-dev-txt', {
-    zoneId: dnsZone.id,
-    name: domain,
-    type: 'TXT',
-    content:
-      'google-site-verification=NZG41fnGV15ayrcCJ6-tS1_Qk-BE6Ynhw25KOLnRV7o',
-    ttl: 3600,
+  const txtRecords = [
+    {
+      name: '_dmarc',
+      ttl: 3600,
+      value:
+        'v=DMARC1;  p=none; rua=mailto:60c475918b8b4cb188e919bd2dd2b1b8@dmarc-reports.cloudflare.net;',
+    },
+    {
+      name: domain,
+      ttl: 3600,
+      value:
+        'google-site-verification=NZG41fnGV15ayrcCJ6-tS1_Qk-BE6Ynhw25KOLnRV7o',
+    },
+    {
+      name: domain,
+      ttl: 3600,
+      value: 'v=spf1 include:_spf.google.com ~all',
+    },
+    {
+      name: 'google._domainkey',
+      ttl: 3600,
+      value:
+        'v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoTwLIgTUUc24nH1+ZENiCsHrUyvzUOuHt2QaoQohikwT8P2F+tJQ+GtVvFlN8WvysFVznFWGpDtFEObwirUc+sNSGyKjPgfIeig9lhA1iyPz8A16UWxA/xcUBZ3lFR2DtYa1PVfsFiGtLVsy4b/dlZ/kZH9oJdpqqudpzVeoUIaC+HYT4izMxMHy1nLDlgFztICozPfWVZZkmmUvD792DcnQIperMOQRnRzhWFGE+EBwOsR1szfSCjc+8h8HjPregN7SsGTSYPYYu0nCZDjjXJhFLiZfP6X5bsaAcKxXYKiiRpClDpf01rOzijP5Dpp16OKE9qy9R/p64Zfl67qgJwIDAQAB',
+    },
+  ];
+
+  txtRecords.forEach((record, index) => {
+    new cloudflare.Record(`zth-dev-txt-${index}`, {
+      zoneId: dnsZone.id,
+      name: record.name,
+      type: 'TXT',
+      content: record.value,
+      ttl: record.ttl,
+    });
   });
 
   const hostnames = [dnsZone.zone, pulumi.interpolate`*.${dnsZone.zone}`];
