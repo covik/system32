8bd526c51bdeac7dc2263fba8b1b15b9945cf9ff
Mate Nakić <mate.nakic@zth.dev>
2026-06-10 05:45:12 +0000

    cromanjonac: add traccar mysql ssql_mode variable
    Matches Traccar documentation
    
    Signed-off-by: GitHub <noreply@github.com>

--- projects/cromanjonac/index.ts
+++ projects/cromanjonac/index.ts
@@ -27,7 +27,7 @@ const fmsMysqlFlags = {
 	autoReconnect: "true",
 	useUnicode: "yes",
 	characterEncoding: "UTF-8",
-	"sessionVariables=sql_require_primary_key": "1",
+	"sessionVariables=sql_mode": "''",
 };
 
 function resources(): unknown {
