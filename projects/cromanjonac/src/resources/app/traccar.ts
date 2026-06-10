b5172105abb21af6508dbae3f426d9a9972c02fa
Mate Nakić <mate.nakic@zth.dev>
2026-06-10 05:24:55 +0000

    deps: fix traccar typo
    Signed-off-by: GitHub <noreply@github.com>

--- projects/cromanjonac/src/resources/app/traccar.ts
+++ projects/cromanjonac/src/resources/app/traccar.ts
@@ -139,7 +139,7 @@ Kontakt je uključen na vozilu $device.name
 								{
 									name: "server",
 									image:
-										"tracar/traccar:6.14.4@sha256:f187e0be41503f1365fec62cb3075c8545fa6b325a5ac47e37667a47e7a31459",
+										"traccar/traccar:6.14.4@sha256:f187e0be41503f1365fec62cb3075c8545fa6b325a5ac47e37667a47e7a31459",
 									imagePullPolicy: "IfNotPresent",
 									ports: [
 										{
