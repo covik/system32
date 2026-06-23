e223884c15e514f8072ec67c911520e6864e1154
Mate Nakić <mate.nakic@zth.dev>
2026-06-23 04:01:48 +0000

    cromanjonac: add parent to cname record
    Signed-off-by: GitHub <noreply@github.com>

--- projects/cromanjonac/src/resources/app/cromanjonac-homepage.ts
+++ projects/cromanjonac/src/resources/app/cromanjonac-homepage.ts
@@ -26,13 +26,19 @@ export class CromanjonacHomePage extends pulumi.ComponentResource {
 			{},
 		);
 
-		new cloudflare.DnsRecord("cname-to-zth.dev", {
-			zoneId: dnsZone.id,
-			name: hostname,
-			type: "CNAME",
-			content: "zth.dev",
-			ttl: 1800,
-		});
+		new cloudflare.DnsRecord(
+			"cname-to-zth.dev",
+			{
+				zoneId: dnsZone.id,
+				name: hostname,
+				type: "CNAME",
+				content: "zth.dev",
+				ttl: 1800,
+			},
+			{
+				parent: this,
+			},
+		);
 
 		const namespace = new k8s.core.v1.Namespace(
 			`${name}-ns`,
